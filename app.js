db396f68691bf31eff30224ce454fafd950101f1
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-01-07 23:42:12 +0100

    function createBoard()

--- app.js
+++ app.js
@@ -48,21 +48,21 @@ const cardArray = [
     img: "images/pizza.png",
   },
 ];
-console.log(cardArray);
-
 cardArray.sort(() => 0.5 - Math.random());
-console.log(cardArray);
 
-const gridDisplay = document.querySelector("#grid");
+const grid = document.querySelector(".grid");
+const resultDisplay = document.querySelector("#result");
+let cardsChosen = [];
+let cardsChosenId = [];
+let cardsWon = [];
 
+//create your board
 function createBoard() {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < cardArray.length; i++) {
     const card = document.createElement("img");
     card.setAttribute("src", "images/blank.png");
     card.setAttribute("data-id", i);
-    gridDisplay.appendChild(card);
-
-    console.log(card, i);
+    card.addEventListener("click", flipCard);
+    grid.appendChild(card);
   }
 }
-createBoard();
