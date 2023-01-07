d07144bb0b5d45f26344d56e8dd2ce74e44ee457
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-01-07 23:45:20 +0100

    else {       cards[optionOneId]

--- app.js
+++ app.js
@@ -82,3 +82,11 @@ function createBoard() {
       cards[optionOneId].removeEventListener('click', flipCard)
       cards[optionTwoId].removeEventListener('click', flipCard)
       cardsWon.push(cardsChosen)
+    } else {
+      cards[optionOneId].setAttribute('src', 'images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'images/blank.png')
+      alert('Sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
